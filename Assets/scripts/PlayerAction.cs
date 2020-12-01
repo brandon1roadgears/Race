b776de49e8ca8587b579aacf41439547d006fcea
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-01 17:55:04 +0700

    Update PlayerAction.cs

--- Assets/scripts/PlayerAction.cs
+++ Assets/scripts/PlayerAction.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private float sideSpeed = 10f;
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * sideSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * sideSpeed * Time.deltaTime);
+        }
     }
 }
