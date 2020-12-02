d25cb8e6eca4c18a15536ec96c3e8f0b6d454eb8
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-02 22:45:11 +0700

    Add events of Play and Exit in ButtonEvent.cs

--- Assets/Scripts/ButtonEvent.cs
+++ Assets/Scripts/ButtonEvent.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonEvent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnStartButtonClick()
     {
-        
+        SceneManager.LoadScene("MainGame");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnExitButtonClick()
     {
-        
+        Application.Quit();
     }
 }
