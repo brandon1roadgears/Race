22eb10c5a00fcf55b7ff994e6b99c27d35aecab9
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-04 00:23:20 +0700

    Add shoot logic in PlayerShoot.cs

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject Bullet;
+    [SerializeField] private Transform ShootPoint;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootLogic();
+        }
+    }
+    
+    private void ShootLogic()
+    {
+        GameObject Bull = Instantiate(Bullet, ShootPoint.position, ShootPoint.rotation);
+        Rigidbody2D rb = Bull.GetComponent<Rigidbody2D>();
+        rb.AddForce(ShootPoint.up, ForceMode2D.Impulse);
+    }
+    
+}
