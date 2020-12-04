f15aad4326cb42594df4b3a93876245b7494d952
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-05 00:12:32 +0700

    Create UfoStats.cs and add stats logic

---
+++ Assets/Scripts/Enemies/UfoStats.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class UfoStats : MonoBehaviour
+{
+    private short HealthPoint = 4;
+    private string[] Collisions = { "Bullet" };
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        foreach (string i in Collisions)
+        {
+            if (col.tag == "Bullet")
+            {
+                --HealthPoint;
+            }
+        }
+        if (HealthPoint == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
