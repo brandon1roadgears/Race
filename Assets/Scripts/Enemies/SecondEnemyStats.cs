3c744d302b929e7f33bc79a4638e54e75031bd57
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-05 00:33:08 +0700

    Add Score increment in stat files

--- Assets/Scripts/Enemies/SecondEnemyStats.cs
+++ Assets/Scripts/Enemies/SecondEnemyStats.cs
@@ -1,20 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SecondEnemyStats : MonoBehaviour
 {
     private short HealthPoint = 2;
     private string[] Collisions = { "Bullet" };
+    private Text Score;
+    void Start()
+    {
+        Score = GameObject.Find("Text(Points)").GetComponent<Text>();
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
         foreach (string i in Collisions)
         {
-            if (col.tag == "Bullet")
+            if (col.tag == i)
             {
                 --HealthPoint;
             }
         }
         if (HealthPoint == 0)
         {
+            short Points = short.Parse(Score.text);
+            ++Points;
+            Score.text = Points.ToString();
             Destroy(this.gameObject);
         }
     }
