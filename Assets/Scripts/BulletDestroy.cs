0b7d69e3bd928c9a1e21e8850c84db78de2e265f
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-04 16:40:48 +0700

    Add destroy logic for BulletDestroy.cs

--- Assets/Scripts/BulletDestroy.cs
+++ Assets/Scripts/BulletDestroy.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BulletDestroy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D col)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(col.tag == "Border")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
