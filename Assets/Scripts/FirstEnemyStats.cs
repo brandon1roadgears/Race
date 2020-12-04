b3f9ae5f8a7a74e77a5f2c46883a5b71e71a2626
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-04 23:40:19 +0700

    Add death condition in Enemystats.cs

---
+++ Assets/Scripts/FirstEnemyStats.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FirstEnemyStats : MonoBehaviour
+{
+    private short HealthPoint = 1;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Bullet")
+        {
+            --HealthPoint;
+        }
+        if (HealthPoint == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
