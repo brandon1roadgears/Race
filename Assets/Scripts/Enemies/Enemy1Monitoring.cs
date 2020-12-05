5348e6169709d027a8f84ad0459c6e89733a8b27
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-05 15:54:23 +0700

    Create monitoringScript and add logic

---
+++ Assets/Scripts/Enemies/Enemy1Monitoring.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Enemy1Monitoring : MonoBehaviour
+{
+    private Transform PlayerPosition;
+
+    void Start()
+    {
+        PlayerPosition = GameObject.Find("Player").GetComponent<Transform>();    
+    }
+
+    void Update()
+    {
+        this.transform.LookAt2D(this.transform.up, PlayerPosition);
+    }
+}
