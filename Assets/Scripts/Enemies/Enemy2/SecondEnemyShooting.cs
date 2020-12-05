b9e0633439f69d0fc50039a8b723bc1b4a7d839d
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-05 16:34:16 +0700

    Create SecondEnemyShooting and add shoot logic

---
+++ Assets/Scripts/Enemies/Enemy2/SecondEnemyShooting.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondEnemyShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject Bullet;
+    private Transform ShootPoint;
+    private bool IsShooting = true;
+    
+    void Start()
+    {
+        ShootPoint = GameObject.Find("Enemy2ShootPoint").GetComponent<Transform>();  
+    }
+
+    void Update()
+    {
+        if (IsShooting)
+        {
+            IsShooting = false;
+            StartCoroutine(ShootLogic());
+        }
+    }
+
+    private IEnumerator ShootLogic()
+    {
+        yield return new WaitForSeconds(1.5f);
+        GameObject Bull = Instantiate(Bullet, ShootPoint.position, ShootPoint.rotation);
+        Rigidbody2D rb = Bull.GetComponent<Rigidbody2D>();
+        rb.AddForce(ShootPoint.up, ForceMode2D.Impulse);
+        IsShooting = true;
+    }
+}
