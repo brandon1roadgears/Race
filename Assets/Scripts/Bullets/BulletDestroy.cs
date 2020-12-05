b6c85d31a02d5dabd5a66624721627d1518dadb9
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-05 20:53:54 +0700

    Add change of mas of tags

--- Assets/Scripts/Bullets/BulletDestroy.cs
+++ Assets/Scripts/Bullets/BulletDestroy.cs
@@ -2,7 +2,16 @@
 
 public class BulletDestroy : MonoBehaviour
 {
-    string[] Collisions = {"Border", "Enemy", "Player"};
+    private string[] Collisions = { "Border", "Enemy", "Player" };
+
+    void Start()
+    {
+        
+        if(this.gameObject.tag == "EnemyBullet")
+        {
+            Collisions[1] = "None";
+        }
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
         foreach(string i in Collisions)
