171393ff969c3c1b20a8da020c44630fef58ed63
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-06 16:31:32 +0700

    Delete void Start()

--- Assets/Scripts/Enemies/Ufo/UfoMovement.cs
+++ Assets/Scripts/Enemies/Ufo/UfoMovement.cs
@@ -4,13 +4,7 @@ public class UfoMovement : MonoBehaviour
 {
     private float radius = 6f;
     private float CenterX = -2f ;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+   
     void FixedUpdate()
     {
         this.transform.position = new Vector2(radius * Mathf.Cos(Time.time) + CenterX, this.transform.position.y);
