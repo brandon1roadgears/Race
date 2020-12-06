f07e7f3b569edb26be56966f1424e919ef5de62d
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-06 17:41:33 +0700

    Add shoot logic in UfoShooting.cs

--- Assets/UfoShooting.cs
+++ Assets/UfoShooting.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UfoShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject Bullet = null;
+    private Transform ShootPoint = null;
+    private bool IsShooting = true;
+    
     void Start()
     {
-        
+        ShootPoint = transform.GetChild(0).GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (IsShooting)
+        {
+            IsShooting = false;
+            StartCoroutine(ShootLogic());
+        }
+    }
+
+    private IEnumerator ShootLogic()
+    {
+        yield return new WaitForSeconds(1.5f);
+        GameObject Bull = Instantiate(Bullet, ShootPoint.position, ShootPoint.rotation);
+        GameObject Bull1 = Instantiate(Bullet, new Vector2(ShootPoint.position.x + 0.76f, ShootPoint.position.y), ShootPoint.rotation);
+        Rigidbody2D rb = Bull.GetComponent<Rigidbody2D>();
+        rb.AddForce(ShootPoint.up, ForceMode2D.Impulse);
+        rb = Bull1.GetComponent<Rigidbody2D>();
+        rb.AddForce(ShootPoint.up, ForceMode2D.Impulse);
+        IsShooting = true;  
     }
 }
