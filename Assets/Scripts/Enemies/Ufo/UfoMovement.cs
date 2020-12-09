2be5e9fb47939700b7f7cfa52c5e5703f4bb7f9c
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-09 15:36:09 +0700

    Add new movement logic in UfoMovement

--- Assets/Scripts/Enemies/Ufo/UfoMovement.cs
+++ Assets/Scripts/Enemies/Ufo/UfoMovement.cs
@@ -2,11 +2,36 @@
 
 public class UfoMovement : MonoBehaviour
 {
-    private float radius = 6f;
-    private float CenterX = -2f ;
-   
+    private int Dir = 0;
+    private float UfoSpeed = 0f;
+
+    private void Start()
+    {
+        Dir = Random.Range(0, 2);
+        UfoSpeed = Random.Range(3f, 7f);
+    }
+
     private void FixedUpdate()
     {
-        this.transform.position = new Vector2(radius * Mathf.Cos(Time.time) + CenterX, this.transform.position.y);
+        MovementLogic();
+        CheckDir();
+    }
+
+    private void MovementLogic()
+    {
+        if (this.transform.position.x > -7.2f && Dir == 0)
+        {
+            this.transform.Translate(Vector2.left * Time.fixedDeltaTime * UfoSpeed);
+        }
+        else if (this.transform.position.x < 3.82f && Dir == 1)
+        {
+            this.transform.Translate(Vector2.right * Time.fixedDeltaTime * UfoSpeed);
+        }
+    }
+
+    private void CheckDir()
+    {
+        if (this.transform.position.x <= -7.2f && Dir == 0) Dir = 1;
+        else if (this.transform.position.x >= 3.82f && Dir == 1) Dir = 0;
     }
 }
