170e878106607764e07a6ac00794ed89e99c4b48
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-10 17:46:29 +0700

    Add start functional in Spawn Behaviour

--- Assets/Scripts/EnemySpawnBehaviour/SpawnBehaviour.cs
+++ Assets/Scripts/EnemySpawnBehaviour/SpawnBehaviour.cs
@@ -1,18 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] 
+    private GameObject Enemy1 = null, Enemy2 = null, Enemy3 = null;
+    private Text ScorePoints = null;
+    private EnemySpawnData[] EsdLight1 = null;
+    private int Score = 0;
+    
+    private struct EnemySpawnData
     {
-        
+        public GameObject EnemyType;
+        public float x;
+        public float y;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        ScorePoints = GameObject.Find("Text(Points)").GetComponent<Text>();
     }
+
+    private void Update()
+    {
+        CheckScore();
+    }
+
+    private void CheckScore()
+    {
+        Score = int.Parse(ScorePoints.text);
+        switch (Score)
+        {
+            case int z when (z >= 0 && z <= 50) :
+                //StartCorutine()
+                break;
+
+            case int z when (z >= 51 && z <= 99):
+                //StartCorutine()
+                break;
+
+            case int z when (z >= 104 && z <= 199):
+                //StartCorutine()
+                break;
+
+            case int z when (z >= 10000 && z <= 10000):
+                //StartCorutine()
+                break;
+
+            case int z when (z >= 10000 && z <= 10000):
+                //StartCorutine()
+                break;
+
+            case int z when (z >= 10000 && z <= 10000):
+                //StartCorutine()
+                break;
+        }
+    }
+
 }
