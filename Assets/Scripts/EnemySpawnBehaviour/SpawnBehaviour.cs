0c38dd9c427e3f73c3ee3fd9d6fcc261c254fb43
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-15 15:40:22 +0700

    Big changes in SpanwBehaviour.cs
    1)Added const vectors for enemies..
    2Added common cond. for switch
    3)Added check of occupied places with help of two arrays.

--- Assets/Scripts/EnemySpawnBehaviour/SpawnBehaviour.cs
+++ Assets/Scripts/EnemySpawnBehaviour/SpawnBehaviour.cs
@@ -6,25 +6,33 @@ public class SpawnBehaviour : MonoBehaviour
 {
     [SerializeField]
     private GameObject Invader1 = null, Invader2 = null, Invader3 = null;
-    private GameObject[] Enemies = new GameObject[3];
     private Text ScorePoints = null;
     private int Score = 0;
     private bool IsCalledSpawn = true;
+    private int CountOfFreePlace = 18;
 
-    internal int CountOfEnemies = 0;
+    private GameObject[] Enemies = null;
+    private Vector2[] ArrayOfPositions = null;
+    private int[] A = null;
+    private int[] B = null;
     
-    private struct EnemySpawnData
-    {
-        public GameObject EnemyType;
-        public Vector2 EnemyPos;
-    }
+    internal int CountOfEnemies = 0;
 
     private void Start()
     {
         ScorePoints = GameObject.Find("Text(Points)").GetComponent<Text>();
+        Enemies = new GameObject[3];
         Enemies[0] = Invader1;
         Enemies[1] = Invader2;
         Enemies[2] = Invader3;
+        ArrayOfPositions = new Vector2[18] { new Vector2(-5.75f, 3.068759f), new Vector2(-4.25f, 3.068759f), new Vector2(-2.75f, 3.068759f),
+                                             new Vector2(-1.25f, 3.068759f), new Vector2(0.25f, 3.068759f), new Vector2(1.75f, 3.068759f),
+
+                                             new Vector2(-5.42f, 1.86f), new Vector2(-3.92f, 1.86f), new Vector2(-2.42f, 1.86f),
+                                             new Vector2(-0.9200001f, 1.86f), new Vector2(0.5799999f, 1.86f), new Vector2(2.08f, 1.86f),
+
+                                             new Vector2(-5.75f, 0.3f), new Vector2(-4.25f, 0.3f), new Vector2(-2.75f, 0.3f),
+                                             new Vector2(-1.25f, 0.3f), new Vector2(0.25f, 0.3f), new Vector2(1.75f, 0.3f) };
     }
 
     private void Update()
@@ -35,60 +43,74 @@ public class SpawnBehaviour : MonoBehaviour
     private void CheckScore()
     {
         Score = int.Parse(ScorePoints.text);
-        switch (Score)
+        if (CheckEnemies() && IsCalledSpawn) 
         {
-            case int Points when (Points >= 0 && Points <= 10 && CheckEnemies() && IsCalledSpawn):
-                IsCalledSpawn = false;
-                StartCoroutine(LightScript1(1));
-                break;
+            IsCalledSpawn = false;
+            switch (Score)
+            {
+                case int Points when (Points >= 0 && Points <= 10):
+                    StartCoroutine(LightScript1(1));
+                    break;
 
-            case int Points when (Points >= 11 && Points <= 20 && CheckEnemies() && IsCalledSpawn):
-                IsCalledSpawn = false;
-                StartCoroutine(LightScript1(2));
-                break;
+                case int Points when (Points >= 11 && Points <= 20):
+                    StartCoroutine(LightScript1(2));
+                    break;
 
-            case int Points when (Points >= 21 && Points <= 30 && CheckEnemies() && IsCalledSpawn):
-                IsCalledSpawn = false;
-                StartCoroutine(LightScript1(3));
-                break;
+                case int Points when (Points >= 21 && Points <= 30):
+                    StartCoroutine(LightScript1(3));
+                    break;
 
-            case int Points when (Points >= 31 && Points <= 40 && CheckEnemies() && IsCalledSpawn):
-                IsCalledSpawn = false;
-                StartCoroutine(LightScript1(4));
-                break;
+                case int Points when (Points >= 31 && Points <= 40):
+                    StartCoroutine(LightScript1(4));
+                    break;
 
-            case int Points when (Points >= 41 && Points <= 50 && CheckEnemies() && IsCalledSpawn):
-                IsCalledSpawn = false;
-                StartCoroutine(LightScript1(5));
-                break;
+                case int Points when (Points >= 41 && Points <= 50):
+                    StartCoroutine(LightScript1(5));
+                    break;
 
-            case int Points when (Points >= 51 && Points <= 60 && CheckEnemies() && IsCalledSpawn):
-                IsCalledSpawn = false;
-                StartCoroutine(LightScript1(6));
-                break;
+                case int Points when (Points >= 51 && Points <= 60):
+                    StartCoroutine(LightScript1(6));
+                    break;
 
-            case int Points when (Points >= 61 && Points <= 70 && CheckEnemies() && IsCalledSpawn):
-                IsCalledSpawn = false;
-                StartCoroutine(LightScript1(7));
-                break;
+                case int Points when (Points >= 61 && Points <= 70):
+                    StartCoroutine(LightScript1(7));
+                    break;
 
-            case int Points when (Points >= 71 && Points <= 80 && CheckEnemies() && IsCalledSpawn):
-                IsCalledSpawn = false;
-                StartCoroutine(LightScript1(8));
-                break;
+                case int Points when (Points >= 71 && Points <= 80):
+                    StartCoroutine(LightScript1(8));
+                    break;
 
-            case int Points when (Points >= 81 && CheckEnemies() && IsCalledSpawn):
-                IsCalledSpawn = false;
-                StartCoroutine(LightScript1(9));
-                break;
+                case int Points when (Points >= 81):
+                    StartCoroutine(LightScript1(9));
+                    break;
+            }
         }
     }
     private IEnumerator LightScript1(int HowMany)
     {
         yield return new WaitForSeconds(1.5f);
-        for(byte i = 0; i < HowMany; ++i)
+        CountOfFreePlace = 18;
+        B = new int[CountOfFreePlace];
+        for (int i = 0; i < CountOfFreePlace; ++i)
         {
-            Instantiate(Enemies[Random.Range(0, 3)], new Vector2(Random.Range(-7f, 3f), Random.Range(3f, 1.2f)), new Quaternion(0f, 0f, 0f, 0f));
+            B[i] = i;
+        }
+        for (byte i = 0; i < HowMany; ++i)
+        {
+            int Rnd = Random.Range(0, CountOfFreePlace);
+            Instantiate(Enemies[Random.Range(0, 3)], ArrayOfPositions[B[Rnd]], new Quaternion(0f, 0f, 0f, 0f));
+            B[Rnd] = -1;
+            A = B;
+            --CountOfFreePlace;
+            B = new int[CountOfFreePlace];
+            for(int j = 0, z = 0; j < CountOfFreePlace + 1; ++j)
+            {
+                if(A[j] != -1)
+                {
+                    B[z] = A[j];
+                    ++z;
+                }
+            }
         }
         IsCalledSpawn = true;
     }
