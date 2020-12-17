b82e1a1a2119a7e9dceb8307a7d63da8173434e8
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-17 23:34:34 +0700

    Added the appearance of score on GameOVerPanel

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -4,14 +4,18 @@ using UnityEngine.UI;
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private AudioClip GameOverSound = null;
+    [SerializeField] private GameObject Panel = null;
+    [SerializeField] private Text EndScore = null;
     private AudioSource SoundPlay = null;
     private Text HealthText = null;
+    private Text Score = null;
     private byte HealthPoint = 5;
 
     private void Start()
     {
         SoundPlay = GameObject.Find("Main Camera").GetComponent<AudioSource>();
         HealthText = GameObject.Find("Text(Hp_points)").GetComponent<Text>();
+        Score = GameObject.Find("Text(Points)").GetComponent<Text>();
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -25,9 +29,10 @@ public class PlayerStats : MonoBehaviour
         }
         if(HealthPoint == 0)
         {
+            EndScore.text = Score.text;
+            Panel.SetActive(true);
             SoundPlay.Stop();
             SoundPlay.PlayOneShot(GameOverSound);
-            Time.timeScale = 0;
             Destroy(this.gameObject);
         }
     }
