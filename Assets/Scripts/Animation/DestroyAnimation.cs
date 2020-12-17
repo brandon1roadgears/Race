cda3733a850945757972cf9a4e88752b8f9d3545
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-17 18:17:35 +0700

    Add spawn of DestroyAnimation instead of animation

---
+++ Assets/Scripts/Animation/DestroyAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DestroyAnimation : MonoBehaviour
+{
+    private Animator DesAnim = null;
+
+    private void Start()
+    {
+        DesAnim = this.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Destroy(this.gameObject, DesAnim.GetCurrentAnimatorClipInfo(0).Length - 0.4f);
+    }
+}
