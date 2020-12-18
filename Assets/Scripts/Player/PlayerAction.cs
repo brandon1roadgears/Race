d9c3dd68e427b0768011e1c681fecd1c1956aa07
brandon1roadgears <danil_timchenko_1997@mail.ru>
2020-12-18 22:30:47 +0700

    Added methods for ButtonEvent for player movement

--- Assets/Scripts/Player/PlayerAction.cs
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -4,6 +4,8 @@ public class PlayerAction : MonoBehaviour
 {
     private float playerSpeed = 10f;
     private float LeftBorder = 0f, RightBorder = 0f;
+    private bool IsMovingLeft = false;
+    private bool IsMovingRight = false;
 
     private void Start()
     {
@@ -41,20 +43,36 @@ public class PlayerAction : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        MovementLogic();
-    }
-
-    private void MovementLogic()
-    {
-        if (Input.GetKey(KeyCode.A) && transform.position.x >= LeftBorder)
+        if (IsMovingLeft && this.transform.position.x >= LeftBorder)
         {
             this.transform.Translate(Vector2.left * playerSpeed * Time.fixedDeltaTime);
         }
-        if (Input.GetKey(KeyCode.D) && this.transform.position.x < RightBorder)
+
+        if(IsMovingRight && this.transform.position.x <= RightBorder)
         {
             this.transform.Translate(Vector2.right * playerSpeed * Time.fixedDeltaTime);
         }
+    }
+
+    public void OnLeiftCklickDown()
+    {
+        IsMovingLeft = true;
+    }
+
+    public void OnRightCklickDown()
+    {
+        IsMovingRight = true;
+    }
+
+    public void OnLeiftCklickUp()
+    {
+        IsMovingLeft = false;
+    }
+
+    public void OnRightCklickUp()
+    {
+        IsMovingRight = false;
     }
 }
