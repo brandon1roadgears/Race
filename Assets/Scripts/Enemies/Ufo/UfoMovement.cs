1228dc0212dd5b282a03cc3d472746f7bcbe6b5c
Danya <brandon1roadgears@users.noreply.github.com>
2020-12-26 15:51:24 +0700

    Changed border values

--- Assets/Scripts/Enemies/Ufo/UfoMovement.cs
+++ Assets/Scripts/Enemies/Ufo/UfoMovement.cs
@@ -4,11 +4,13 @@ public class UfoMovement : MonoBehaviour
 {
     private int Dir = 0;
     private float UfoSpeed = 0f;
+    private float LeftBorder =-3.04f;
+    private float RightBorder = 2.46f;
 
     private void Start()
     {
         Dir = Random.Range(0, 2);
-        UfoSpeed = Random.Range(3f, 7f);
+        UfoSpeed = Random.Range(1.5f, 5.5f);
     }
 
     private void FixedUpdate()
@@ -19,11 +21,11 @@ public class UfoMovement : MonoBehaviour
 
     private void MovementLogic()
     {
-        if (this.transform.position.x > -7.2f && Dir == 0)
+        if (this.transform.position.x > LeftBorder && Dir == 0)
         {
             this.transform.Translate(Vector2.left * Time.fixedDeltaTime * UfoSpeed);
         }
-        else if (this.transform.position.x < 3.82f && Dir == 1)
+        else if (this.transform.position.x < RightBorder && Dir == 1)
         {
             this.transform.Translate(Vector2.right * Time.fixedDeltaTime * UfoSpeed);
         }
@@ -31,7 +33,7 @@ public class UfoMovement : MonoBehaviour
 
     private void CheckDir()
     {
-        if (this.transform.position.x <= -7.2f && Dir == 0) Dir = 1;
-        else if (this.transform.position.x >= 3.82f && Dir == 1) Dir = 0;
+        if (this.transform.position.x <= LeftBorder && Dir == 0) Dir = 1;
+        else if (this.transform.position.x >= RightBorder && Dir == 1) Dir = 0;
     }
 }
