5b529b6a090edd83d1f94c641cd1a479958cd91b
Danya <brandon1roadgears@users.noreply.github.com>
2020-12-26 15:56:31 +0700

    Create UiMainGame and added SetActive for panels of control type

---
+++ Assets/Scripts/UI/UiMainGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class UiMainGame : MonoBehaviour
+{
+    [SerializeField] private GameObject Configuration1 = null;
+    [SerializeField] private GameObject Configuration2 = null;
+
+    private void Start()
+    {
+        ButtonEvent BE = GetComponent<ButtonEvent>();
+        if(BE.GetValueOfConfiguration())
+        {
+            Configuration1.SetActive(true);
+            Configuration2.SetActive(false);
+        }
+        else
+        {
+            Configuration1.SetActive(false);
+            Configuration2.SetActive(true);
+        }
+    }
+}
+
