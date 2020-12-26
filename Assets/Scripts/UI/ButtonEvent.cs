ce30ebc5c1f07984eaa12a2408bd91850b9e2a77
Danya <brandon1roadgears@users.noreply.github.com>
2020-12-26 15:55:15 +0700

    Added definition of Control type

--- Assets/Scripts/UI/ButtonEvent.cs
+++ Assets/Scripts/UI/ButtonEvent.cs
@@ -3,6 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class ButtonEvent : MonoBehaviour
 {
+    private static bool ControlType = true;
+
+    public bool GetValueOfConfiguration()
+    {
+        return ControlType;
+    }
+
+    public void OnControlTypeChange1()
+    {
+        ControlType = true;
+    }
+
+    public void OnControlTypeChange2()
+    {
+        ControlType = false;
+    }
+
     public void OnStartButtonClick()
     {
         SceneManager.LoadScene("MainGame");
