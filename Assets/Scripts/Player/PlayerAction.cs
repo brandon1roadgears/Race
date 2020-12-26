eb7f38cbbdcfc561d42680e2565f100f1a1ddfd3
Danya <brandon1roadgears@users.noreply.github.com>
2020-12-26 15:54:48 +0700

    Added border values depending on resolution

--- Assets/Scripts/Player/PlayerAction.cs
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -1,24 +1,58 @@
 using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
-{    
-    private float playerSpeed = 10f;
-    private float LeftBorder = 0f, RightBorder = 0f;
+{ 
+    private float playerSpeed = 6f;
+    private float LeftBorder = -3.26f, RightBorder = 3.26f;
     private bool IsMovingLeft = false;
     private bool IsMovingRight = false;
+    
 
     private void Start()
     {
+        switch(Camera.main.pixelWidth)
+        {
+            case 800:
+                LeftBorder = -3.26f;
+                RightBorder = 3.26f;
+                break;
+            
+            case 1280:
+                LeftBorder = -3.47f;
+                RightBorder = 3.47f;
+                break;
+
+            case 1920:
+                LeftBorder = -3.47f;
+                RightBorder = 3.47f;
+                break;
+
+            case 2160:
+                LeftBorder = -3.94f;
+                RightBorder = 3.94f;
+                break;
+            
+            case 2560:
+                LeftBorder = -3.47f;
+                RightBorder = 3.47f;
+                break;
+
+            case 2960:
+                LeftBorder = -4.03f;
+                RightBorder = 4.03f;
+                break;
+        }
+
     }
 
     private void Update()
     {
-        if (IsMovingLeft && this.transform.position.x >= LeftBorder)
+        if (IsMovingLeft && this.transform.position.x > LeftBorder)
         {
             this.transform.Translate(Vector2.left * playerSpeed * Time.fixedDeltaTime);
         }
 
-        if(IsMovingRight && this.transform.position.x <= RightBorder)
+        if(IsMovingRight && this.transform.position.x < RightBorder)
         {
             this.transform.Translate(Vector2.right * playerSpeed * Time.fixedDeltaTime);
         }
