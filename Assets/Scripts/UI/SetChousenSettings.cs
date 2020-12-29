fc226adaae13081f5bda69d178c17e50047c02e6
Danya <brandon1roadgears@users.noreply.github.com>
2020-12-29 22:55:00 +0700

    Create for setup state of chousen configuration in setting panel

---
+++ Assets/Scripts/UI/SetChousenSettings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SetChousenSettings : MonoBehaviour
+{
+    [SerializeField] private GameObject ConfigurationPane1 = null;
+    [SerializeField] private GameObject ConfigurationPane2 = null;
+    private ButtonEvent Be = null;
+    private void Start()
+    {        
+        Be = GameObject.Find("Canvas").GetComponent<ButtonEvent>();
+
+        if(Be.GetValueOfConfiguration()){
+            ConfigurationPane1.SetActive(true);
+            ConfigurationPane2.SetActive(false);
+        }
+        else
+        {
+            ConfigurationPane1.SetActive(false);
+            ConfigurationPane2.SetActive(true);
+        }
+    }
+}
