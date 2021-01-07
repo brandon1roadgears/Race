58ed6b4e0d71ef645440773966dad9f812babbe7
Danya <brandon1roadgears@users.noreply.github.com>
2021-01-07 18:01:29 +0700

    Added LoadSystem for ReordPanel in MainMenu

--- Assets/Scripts/Save/LoadRecords.cs
+++ Assets/Scripts/Save/LoadRecords.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using Structs;
 
 public class LoadRecords : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Text [] Records = new Text[10];
+    RecordsArray _RecordsArray;
+    private string _Path = "";
     void Start()
     {
-        
-    }
+         #if  UNITY_ANDROID && !UNITY_EDITOR
+            _Path = Path.Combine(Application.persistentDataPath, "Save.json");
+        #else
+            _Path = Path.Combine(Application.dataPath, "Save.json");
+        #endif
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(!File.Exists(_Path))
+        {
+            return;
+        }
+        _RecordsArray = JsonUtility.FromJson<RecordsArray>(File.ReadAllText(_Path));
+        for(int i = 9; i > -1; --i)
+        {
+            if(_RecordsArray.Records[i] != -1)
+            {
+                Records[9 - i].text = _RecordsArray.Records[i].ToString();
+            }
+        }
     }
-}
+} 
