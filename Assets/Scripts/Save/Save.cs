a44b2035ae29367b33ecccf2713730c39abf9de9
Danya <brandon1roadgears@users.noreply.github.com>
2021-01-07 14:21:29 +0700

    Create SaveSystem in Save.cs

--- Assets/Scripts/Save/Save.cs
+++ Assets/Scripts/Save/Save.cs
@@ -2,18 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
+using Structs;
 
 public class Save : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{    
+
+    private string _Path = "";
+    RecordsArray _RecordsArray;
+
+    private void Awake()
     {
-        
+        #if  UNITY_ANDROID && !UNITY_EDITOR
+            _Path = Path.Combine(Application.persistentDataPath, "Save.json");
+        #else
+            _Path = Path.Combine(Application.dataPath, "Save.json");
+        #endif
+    }
+    private void Start()
+    {
+        if(File.Exists(_Path))
+        {
+            _RecordsArray = JsonUtility.FromJson<RecordsArray>(File.ReadAllText(_Path));
+        }
+        else
+        {
+            _RecordsArray = new RecordsArray{};
+            _RecordsArray.Records = new int[10] {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    internal void SaveResult(int Record)
     {
-        
+        int LastIndex = 0;
+        for(int i = 0; i < 10; ++i)
+        {
+            if(Record >= _RecordsArray.Records[i])
+            {
+                    LastIndex = i;
+            }
+        }
+        for(int i = 0; i < LastIndex; ++i)
+        {
+            _RecordsArray.Records[i] = _RecordsArray.Records[i+1];
+        }
+
+        _RecordsArray.Records[LastIndex] = Record;
+        File.WriteAllText(_Path, JsonUtility.ToJson(_RecordsArray, true));
     }
 }
+
