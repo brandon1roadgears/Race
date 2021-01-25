6f152bf23ac6c06cd17d372290ac21985b380ba4
Danya <brandon1roadgears@users.noreply.github.com>
2021-01-25 22:01:23 +0700

    Added definition of SaveRecords.json path, changed load of records and added load of settings

--- Assets/Scripts/Save/LoadRecords.cs
+++ Assets/Scripts/Save/LoadRecords.cs
@@ -7,26 +7,48 @@ public class LoadRecords : MonoBehaviour
 {
     [SerializeField] Text [] Records = new Text[10];
     RecordsArray _RecordsArray;
-    private string _Path = "";
+    RecordsSettings _RecordsSettings;
+    private string _PathForRecords = "";
+    private string _PathForSettings = "";
     void Start()
     {
-         #if  UNITY_ANDROID && !UNITY_EDITOR
-            _Path = Path.Combine(Application.persistentDataPath, "Save.json");
+        #if  UNITY_ANDROID && !UNITY_EDITOR
+            _PathForRecords = Path.Combine(Application.persistentDataPath, "SaveRecords.json");
+            _PathForSettings = Path.Combine(Application.persistentDataPath, "SaveSettings.json");
         #else
-            _Path = Path.Combine(Application.dataPath, "Save.json");
+            _PathForRecords = Path.Combine(Application.dataPath, "SaveRecords.json");
+            _PathForSettings = Path.Combine(Application.dataPath, "SaveSettings.json");
         #endif
 
-        if(!File.Exists(_Path))
+        if(File.Exists(_PathForRecords))
         {
-            return;
-        }
-        _RecordsArray = JsonUtility.FromJson<RecordsArray>(File.ReadAllText(_Path));
-        for(int i = 9; i > -1; --i)
-        {
-            if(_RecordsArray.Records[i] != -1)
+            _RecordsArray = JsonUtility.FromJson<RecordsArray>(File.ReadAllText(_PathForRecords));
+            for(int i = 9; i > -1; --i)
             {
-                Records[9 - i].text = _RecordsArray.Records[i].ToString();
+                if(_RecordsArray.Records[i] != -1)
+                {
+                    Records[9 - i].text = _RecordsArray.Records[i].ToString();
+                }
             }
+        }
+        else
+        {
+            _RecordsArray = new RecordsArray{};
+            _RecordsArray.Records = new int[10] {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+            File.WriteAllText(_PathForRecords, JsonUtility.ToJson(_RecordsArray, true));
+        }
+
+        if(File.Exists(_PathForSettings))
+        {
+            _RecordsSettings = JsonUtility.FromJson<RecordsSettings>(File.ReadAllText(_PathForSettings));
+        }
+        else
+        {
+            _RecordsSettings = new RecordsSettings{};
+            _RecordsSettings.MusicVolume = 1.0f;
+            _RecordsSettings.SoundsVolume = 1.0f;
+            _RecordsSettings.TypeOfControl = 1;
+            File.WriteAllText(_PathForSettings, JsonUtility.ToJson(_RecordsSettings, true));
         }
     }
 } 
