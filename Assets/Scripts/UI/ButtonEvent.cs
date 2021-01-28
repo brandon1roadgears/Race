301c2249f216bf10f1c1503ec102e490dc58b1a0
Danya <brandon1roadgears@users.noreply.github.com>
2021-01-28 18:24:07 +0700

    Added setting volume level in Settings Menu

--- Assets/Scripts/UI/ButtonEvent.cs
+++ Assets/Scripts/UI/ButtonEvent.cs
@@ -1,10 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class ButtonEvent : MonoBehaviour
 {
+    [SerializeField] private AudioMixer MusicMixer = null;
+    [SerializeField] private AudioMixer SoundMixer = null;
+    [SerializeField] private AudioClip TestSound = null;
+    [SerializeField] private AudioSource MenuListener = null;
     private static byte ControlType = 1;
 
+    public void SetVolumeOfMusic(float Volume)
+    {
+        MusicMixer.SetFloat("MusicVolume", Mathf.Log10(Volume) * 20);
+    }
+    public void SetVolumeOfSounds(float Volume)
+    {
+        SoundMixer.SetFloat("SoundVolume", Mathf.Log10(Volume) * 20);
+    }
+
+    public void CheckSoundVolume()
+    {
+        MenuListener.PlayOneShot(TestSound);
+    }
+    
     public byte GetValueOfConfiguration()
     {
         return ControlType;
