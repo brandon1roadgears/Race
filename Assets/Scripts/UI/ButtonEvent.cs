54369d77f91342ddc0e41bece0b85664bbd43af8
Danya <brandon1roadgears@users.noreply.github.com>
2021-01-31 15:33:57 +0700

    Deleted useless button-events

--- Assets/Scripts/UI/ButtonEvent.cs
+++ Assets/Scripts/UI/ButtonEvent.cs
@@ -8,7 +8,6 @@ public class ButtonEvent : MonoBehaviour
     [SerializeField] private AudioMixer SoundMixer = null;
     [SerializeField] private AudioClip TestSound = null;
     [SerializeField] private AudioSource MenuListener = null;
-    private static byte ControlType = 1;
 
     public void SetVolumeOfMusic(float Volume)
     {
@@ -24,50 +23,13 @@ public class ButtonEvent : MonoBehaviour
         MenuListener.PlayOneShot(TestSound);
     }
     
-    public byte GetValueOfConfiguration()
-    {
-        return ControlType;
-    }
-
-    public void OnControlTypeChange1()
-    {
-        ControlType = 1;
-    }
-
-    public void OnControlTypeChange2()
-    {
-        ControlType = 2;
-    }
-
-    public void OnControlTypeChange3()
-    {
-        ControlType = 3;
-    }
-
     public void OnStartButtonClick()
     {
-        Time.timeScale = 1;
         SceneManager.LoadScene("MainGame");
     }
 
     public void OnExitButtonClick()
     {
         Application.Quit();
-    }
-
-    public void OnHomeClick()
-    {
-        Time.timeScale = 1;
-        SceneManager.LoadScene("Menu");
-    }
-
-    public void OnPauseButtonClick()
-    {
-        Time.timeScale = 0;
-    }
-
-    public void OnResumeButtonClick()
-    {
-        Time.timeScale = 1;
     }
 }
