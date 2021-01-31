59bd809b104549d4df002453e34df71a7f86fe4d
Danya <brandon1roadgears@users.noreply.github.com>
2021-01-31 15:34:53 +0700

    Deleted useless creating and checking save files

--- Assets/Scripts/Save/Save.cs
+++ Assets/Scripts/Save/Save.cs
@@ -1,62 +1,50 @@
 ﻿using UnityEngine;
 using System.IO;
-using Structs;
 
 public class Save : MonoBehaviour
-{    
-    private string _PathForRecords = "";
-    private string _PathForSettings = "";
-    RecordsArray _RecordsArray;
-    RecordsSettings _RecordsSettings;
-
+{
+    private CreateSaveFiles _CreateSaveFiles;
     private void Awake()
     {
-        #if  UNITY_ANDROID && !UNITY_EDITOR
-            _PathForRecords = Path.Combine(Application.persistentDataPath, "SaveRecords.json");
-            _PathForSettings = Path.Combine(Application.persistentDataPath, "SaveSettings.json");
-        #else
-            _PathForRecords = Path.Combine(Application.dataPath, "SaveRecords.json");
-            _PathForSettings = Path.Combine(Application.dataPath, "SaveSettings.json");
-        #endif
-        
-        _RecordsSettings = JsonUtility.FromJson<RecordsSettings>(File.ReadAllText(_PathForSettings));
-        _RecordsArray = JsonUtility.FromJson<RecordsArray>(File.ReadAllText(_PathForRecords));
+        _CreateSaveFiles = GetComponent<CreateSaveFiles>();
     }
+
     internal void SaveResult(int Record)
     {
+        
         int LastIndex = 0;
         for(int i = 0; i < 10; ++i)
         {
-            if(Record >= _RecordsArray.Records[i])
+            if(Record >= _CreateSaveFiles._RecordsArray.Records[i])
             {
                     LastIndex = i;
             }
         }
         for(int i = 0; i < LastIndex; ++i)
         {
-            _RecordsArray.Records[i] = _RecordsArray.Records[i+1];
+            _CreateSaveFiles._RecordsArray.Records[i] = _CreateSaveFiles._RecordsArray.Records[i+1];
         }
 
-        _RecordsArray.Records[LastIndex] = Record;
-        File.WriteAllText(_PathForRecords, JsonUtility.ToJson(_RecordsArray, true));
+        _CreateSaveFiles._RecordsArray.Records[LastIndex] = Record;
+        File.WriteAllText(_CreateSaveFiles._PathForRecords, JsonUtility.ToJson(_CreateSaveFiles._RecordsArray, true));
     }
 
     public void SaveSoundSettings(float Volume)
     {
-        _RecordsSettings.SoundsVolume = Volume;
-        File.WriteAllText(_PathForSettings, JsonUtility.ToJson(_RecordsSettings, true));
+        _CreateSaveFiles._RecordsSettings.SoundsVolume = Volume;
+        File.WriteAllText(_CreateSaveFiles._PathForSettings, JsonUtility.ToJson(_CreateSaveFiles._RecordsSettings, true));
     }
 
     public void SaveMusicSettings(float Volume)
     {
-        _RecordsSettings.MusicVolume = Volume;
-        File.WriteAllText(_PathForSettings, JsonUtility.ToJson(_RecordsSettings, true));
+        _CreateSaveFiles._RecordsSettings.MusicVolume = Volume;
+        File.WriteAllText(_CreateSaveFiles._PathForSettings, JsonUtility.ToJson(_CreateSaveFiles._RecordsSettings, true));
     }
 
     public void SaveChousenControlType(int NumberOfConfiguration)
     {
-        _RecordsSettings.TypeOfControl = NumberOfConfiguration;
-        File.WriteAllText(_PathForSettings, JsonUtility.ToJson(_RecordsSettings, true));
+        _CreateSaveFiles._RecordsSettings.TypeOfControl = NumberOfConfiguration;
+        File.WriteAllText(_CreateSaveFiles._PathForSettings, JsonUtility.ToJson(_CreateSaveFiles._RecordsSettings, true));
     }
 }
 
