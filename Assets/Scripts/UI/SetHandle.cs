81f5c8dd68f2e2f82ea626dd3f6777db1363d7b7
Danya <brandon1roadgears@users.noreply.github.com>
2021-02-05 22:28:32 +0700

    Create script for sending link on chousen joystik's handle

---
+++ Assets/Scripts/UI/SetHandle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SetHandle : MonoBehaviour
+{
+    [SerializeField] private Transform Handle = null;
+    private PlayerAction _PlayerAction = null;
+
+    private void Awake()
+    {
+        _PlayerAction = GameObject.Find("Player").GetComponent<PlayerAction>();
+    }
+
+    private void OnEnable()
+    {
+        _PlayerAction.MainHandle = Handle;
+    }
+}
