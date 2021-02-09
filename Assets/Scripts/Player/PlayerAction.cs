5b0b5d8e3add354636e0be8d2dd75a37e6452b22
Danya <brandon1roadgears@users.noreply.github.com>
2021-02-09 16:15:16 +0700

    Changed value of joystick's border

--- Assets/Scripts/Player/PlayerAction.cs
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -2,31 +2,31 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 { 
-    [SerializeField] private GameObject Handle = null;
-    private float playerSpeed = 6f;
+    private Camera Cam;
+    private ButtonEvent ButEv = null;
+    internal Transform MainHandle = null;
+    private float playerSpeed = 5f;
     private float LeftBorder = -3.26f, RightBorder = 3.26f;
     private bool IsMovingLeft = false;
     private bool IsMovingRight = false;
-    private Camera Cam;
 
-    private ButtonEvent ButEv = null;
-
-    private void Start()
+    private void Awake()
     {
         Cam = Camera.main;
         LeftBorder = Cam.ScreenToWorldPoint(Cam.transform.position).x + 0.1f;
         RightBorder = -Cam.ScreenToWorldPoint(Cam.transform.position).x - 0.1f;
         ButEv = GameObject.Find("Canvas").GetComponent<ButtonEvent>();
+        if(!MainHandle) MainHandle = GetComponent<Transform>();
     }
 
     private void Update()
     {
-        if((Handle.transform.localPosition.x < 0 || IsMovingLeft) && this.transform.position.x > LeftBorder)
+        if((MainHandle.localPosition.x <= -175 || IsMovingLeft) && this.transform.position.x > LeftBorder)
         {
             this.transform.Translate(Vector2.left * playerSpeed * Time.fixedDeltaTime);
         }
 
-        if((Handle.transform.localPosition.x > 0 || IsMovingRight) && this.transform.position.x < RightBorder)
+        if((MainHandle.localPosition.x >= 175 || IsMovingRight) && this.transform.position.x < RightBorder)
         {
             this.transform.Translate(Vector2.right * playerSpeed * Time.fixedDeltaTime);
         }
