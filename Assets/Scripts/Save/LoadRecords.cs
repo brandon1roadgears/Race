11911f7a977bfc596bc8c01e4fd1f229388c4428
Danya <brandon1roadgears@users.noreply.github.com>
2021-02-15 17:07:55 +0700

    Cleaned code and changed location of CreateSaveFiles

--- Assets/Scripts/Save/LoadRecords.cs
+++ Assets/Scripts/Save/LoadRecords.cs
@@ -4,14 +4,13 @@ using UnityEngine.UI;
 public class LoadRecords : MonoBehaviour
 {
     [SerializeField] Text [] Records = new Text[10];
-    [SerializeField] private Slider MusicSlider;
-    [SerializeField] private Slider SoundSlider;
+    [SerializeField] private Slider MusicSlider = null, SoundSlider = null;
     [SerializeField] private GameObject[] ChousenConfiguration = new GameObject[10];
     private CreateSaveFiles _CreateSaveFiles;
 
     void Start()
     {
-        _CreateSaveFiles = this.GetComponent<CreateSaveFiles>();   
+        _CreateSaveFiles = Camera.main.GetComponent<CreateSaveFiles>();   
         for(int i = 9; i > -1; --i)
         {
             if(_CreateSaveFiles._RecordsArray.Records[i] != -1)
