c50ad5d4cf13adbe751e9b99f427f4a6681a4c61
Danya <brandon1roadgears@users.noreply.github.com>
2021-02-28 16:49:21 +0700

    Added definition of default values for video parameters

--- Assets/Scripts/Save/CreateSaveFiles.cs
+++ Assets/Scripts/Save/CreateSaveFiles.cs
@@ -39,7 +39,9 @@ public class CreateSaveFiles : MonoBehaviour
             {
                 TypeOfControl = 0,
                 MusicVolume = 1.0f,
-                SoundsVolume = 1.0f
+                SoundsVolume = 1.0f,
+                isBloom = true,
+                isLense = true
             };
             File.WriteAllText(_PathForSettings, JsonUtility.ToJson(_RecordsSettings, true));
         }
