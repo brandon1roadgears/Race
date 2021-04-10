53e9f6410e7cfb80715e71f009314e2f170ec907
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:16:18 +0700

    Added scriptable object field and setting values from this field

--- Assets/Scripts/Enemies/Common/EnemyShooting.cs
+++ Assets/Scripts/Enemies/Common/EnemyShooting.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using ScriptableStatsObject;
 
 public class EnemyShooting : MonoBehaviour
 {
-    [SerializeField] private GameObject Bullet = null;
-    private Transform ShootPoint = null;
-    private float WaitingTime = 0.0f;
+    [SerializeField] private float WaitingTime = 0.0f;
+    private EnemyStatsScOb _EnemyStats = null;
+    private Transform[] ShootPoints = null;
     private bool IsShooting = true;
 
     private void Start()
     {
-        ShootPoint = transform.GetChild(0).GetComponent<Transform>();
-        WaitingTime = Random.Range(0.8f, 2.5f);
+        _EnemyStats = this.GetComponent<EnemyStats>().enemyStats;
+        ShootPoints = this.GetComponentsInChildren<Transform>();
+        if(WaitingTime == 0.0f)
+        {
+            WaitingTime = Random.Range(0.8f, 2.5f);
+        }
     }
-
     private void Update()
     {
         if (IsShooting)
@@ -26,9 +30,37 @@ public class EnemyShooting : MonoBehaviour
     private IEnumerator ShootLogic()
     {
         yield return new WaitForSeconds(WaitingTime);
-        GameObject Bull = Instantiate(Bullet, ShootPoint.position, ShootPoint.rotation);
-        Rigidbody2D rb = Bull.GetComponent<Rigidbody2D>();
-        rb.AddForce(ShootPoint.up, ForceMode2D.Impulse);
+        GameObject bullet = null;
+        Rigidbody2D bulletRigidBody = null;
+        SpriteRenderer bulletSpriteRenderer = null;
+        for(int i = 1; i < ShootPoints.Length; ++i)
+        {
+            SpawnBullet(ref bullet, ref bulletRigidBody, ref bulletSpriteRenderer, i);
+            SetBulletPropeties(bullet, bulletRigidBody, bulletSpriteRenderer);
+            Shoot(bulletRigidBody, i);
+        }
         IsShooting = true;
+    }
+
+    private void SpawnBullet(ref GameObject _bullet, ref Rigidbody2D _bulletRigidBody, ref SpriteRenderer _bulletSpriteRenderer, int index)
+    {
+        _bullet = Instantiate(_EnemyStats.bullet, ShootPoints[index].transform.position, ShootPoints[index].transform.rotation);
+        _bulletRigidBody = _bullet.AddComponent<Rigidbody2D>();
+        _bulletSpriteRenderer = _bullet.AddComponent<SpriteRenderer>();
+
+    }
+    private void SetBulletPropeties(GameObject _bullet, Rigidbody2D _bulletRigodBody, SpriteRenderer _bulletSpriteRenderer)
+    {
+        _bullet.tag = _EnemyStats.bulletTag;
+        _bullet.transform.localScale = _EnemyStats.bulletScale;
+        _bulletSpriteRenderer.sprite = _EnemyStats.bulletSprite;
+        _bulletSpriteRenderer.color = _EnemyStats.bulletColor;
+        _bulletRigodBody.mass = _EnemyStats.bulletMass;
+        _bulletRigodBody.angularDrag = 0.0f;
+        _bulletRigodBody.gravityScale = 0.0f;
+    }
+    private void Shoot(Rigidbody2D _bulletRigidBody, int index)
+    {
+        _bulletRigidBody.AddForce(ShootPoints[index].up, ForceMode2D.Impulse);
     }
 }
