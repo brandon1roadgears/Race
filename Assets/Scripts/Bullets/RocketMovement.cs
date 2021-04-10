f28daf69656abac7389ddbf8017dcd7cc64bb991
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:12:27 +0700

    Created script for moving rocket to target

---
+++ Assets/Scripts/Bullets/RocketMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    private Transform LookAtObject = null;
+    private float Speed = 4f;
+
+    private void Update()
+    {
+        this.transform.Translate(Vector2.up * Time.deltaTime * Speed);
+        if(LookAtObject != null)
+        {
+            this.transform.LookAt2D(this.transform.up, LookAtObject);
+        }
+    }
+
+    internal Transform _LookAtObject
+    {
+        set {LookAtObject = value;}
+    }
+}
