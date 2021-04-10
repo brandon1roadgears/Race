ba0315a59ee583c5f08074138ee7bb0b6616f8c6
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:13:24 +0700

    Created script for spawn rocket's traces

---
+++ Assets/Scripts/Bullets/Tracer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Tracer : MonoBehaviour
+{
+    [SerializeField] private GameObject Trace = null;
+    private bool IsSpawnTrace = true;
+
+    private void Update()
+    {
+        if(IsSpawnTrace)
+        {
+            IsSpawnTrace = false;
+            StartCoroutine(LeaveTrace());
+        }
+    }
+    
+    private IEnumerator LeaveTrace()
+    {
+        yield return new WaitForSeconds(0.08f);
+        Instantiate(Trace, this.transform.position, this.transform.rotation);
+        IsSpawnTrace = true;
+    }
+}
