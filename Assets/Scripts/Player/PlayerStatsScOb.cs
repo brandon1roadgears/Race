f6f4df5add43319060f9fd2daec96ebe5b11a772
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:32:45 +0700

    Created scriptable object for PlayerStats and  PlayerBullet

---
+++ Assets/Scripts/Player/PlayerStatsScOb.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace ScriptablePlayerStatsObject
+{
+    [CreateAssetMenu(fileName = "PlayerStats", menuName = "ScriptableObjects/Player", order = 0)]
+    public class PlayerStatsScOb : ScriptableObject
+    {
+        [Header("PlayerStats")]
+        [SerializeField] Sprite ShipSkin;
+        [SerializeField] private GameObject DestroyAnimation;
+        [SerializeField] private Color DestroyColor;
+        [SerializeField] private int Healthpoint;
+        [SerializeField] private string[] Collisions;
+
+        [Header("PlayerBulletStats")]
+        [SerializeField] private GameObject Bullet;
+        [SerializeField] private Sprite BulletSprite;
+        [SerializeField] private Vector2 BulletScale;
+        [SerializeField] private Color BulletColor;
+        [SerializeField] private float BulletMass;
+        [SerializeField] private string BulletTag;
+
+        public GameObject bullet
+        {
+            get {return Bullet;}
+        }
+        public GameObject destroyAnimation
+        {
+            get {return DestroyAnimation;}
+        }
+        public Sprite bulletSprite
+        {
+            get {return BulletSprite;}
+        }
+        public Sprite shipSkin
+        {
+            get {return ShipSkin;}
+        }
+        public Vector2 bulletScale
+        {
+            get {return BulletScale;}
+        }
+        public Color bulletColor
+        {
+            get {return BulletColor;}
+        }
+        public Color destroyColor
+        {
+            get {return DestroyColor;}
+        }
+        public float bulletMass
+        {
+            get {return BulletMass;}
+        }
+        public string bulletTag
+        {
+            get {return BulletTag;}
+        }
+        public string[] collisions
+        {
+            get {return Collisions;}
+        }
+        public int healthPoint
+        {
+            get {return Healthpoint;}
+        }
+    }
+}
