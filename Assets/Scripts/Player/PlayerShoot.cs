0aadc309775ee78413be7e31479be1fa43251d8b
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:34:14 +0700

    Added scriptable object field and set values from this file

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,24 +1,58 @@
 ﻿using UnityEngine;
+using ScriptablePlayerStatsObject;
 
 public class PlayerShoot : MonoBehaviour
 {
-    [SerializeField] private GameObject Bullet = null;
-    [SerializeField] private AudioClip ShootSound = null;
-    private Transform ShootPoint = null;
-    private AudioSource SoundPlay = null;
+    private PlayerStatsScOb _PlayerStats = null;
+    private PlayerParameters _PlayerParameters = null;
+    private Transform[] ShootPoints = null;
 
-
-    private void Start()
+    private void Awake()
     {
-        ShootPoint = GameObject.Find("PlayerShootPoint").GetComponent<Transform>();
-        SoundPlay = GameObject.Find("SoundPoint").GetComponent<AudioSource>();
+        ShootPoints = this.GetComponentsInChildren<Transform>();
+        _PlayerParameters = this.GetComponent<PlayerParameters>();
+        _PlayerStats = this.GetComponent<PlayerStats>().playerStatsScOb;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ClickForShoot();
+        }
     }
 
     public void ClickForShoot()
     {
-            SoundPlay.PlayOneShot(ShootSound);
-            GameObject Bull = Instantiate(Bullet, ShootPoint.position, ShootPoint.rotation);
-            Rigidbody2D rb = Bull.GetComponent<Rigidbody2D>();
-            rb.AddForce(ShootPoint.up, ForceMode2D.Impulse);
+        _PlayerParameters.soundPlay.PlayOneShot(_PlayerParameters.shootSound);
+        GameObject bullet = null;
+        Rigidbody2D bulletRigidBody = null;
+        SpriteRenderer bulletSpriteRenderer = null;
+        for(int i = 1; i < ShootPoints.Length; ++i)
+        {   
+            SpawnBullet(ref bullet, ref bulletRigidBody, ref bulletSpriteRenderer, i);
+            SetBulletPropeties(bullet, bulletRigidBody, bulletSpriteRenderer);
+            Shoot(bulletRigidBody, i);
+        }
+    }
+    private void SpawnBullet(ref GameObject _bullet, ref Rigidbody2D _bulletRigidBody, ref SpriteRenderer _bulletSpriteRenderer, int index)
+    {
+        _bullet = Instantiate(_PlayerStats.bullet, ShootPoints[index].transform.position, ShootPoints[index].transform.rotation);
+        _bulletRigidBody = _bullet.AddComponent<Rigidbody2D>();
+        _bulletSpriteRenderer = _bullet.AddComponent<SpriteRenderer>();
+    }
+    private void SetBulletPropeties(GameObject _bullet, Rigidbody2D _bulletRigodBody, SpriteRenderer _bulletSpriteRenderer)
+    {
+        _bullet.tag = _PlayerStats.bulletTag;
+        _bullet.transform.localScale = _PlayerStats.bulletScale;
+        _bulletSpriteRenderer.sprite = _PlayerStats.bulletSprite;
+        _bulletSpriteRenderer.color = _PlayerStats.bulletColor;
+        _bulletRigodBody.mass = _PlayerStats.bulletMass;
+        _bulletRigodBody.angularDrag = 0.0f;
+        _bulletRigodBody.gravityScale = 0.0f;
+    }
+    private void Shoot(Rigidbody2D _bulletRigidBody, int index)
+    {
+        _bulletRigidBody.AddForce(ShootPoints[index].up, ForceMode2D.Impulse);
     }
 }
