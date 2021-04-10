16de292081989a29d05d18e347b1044510ec09a7
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:21:02 +0700

    Cretaed file for moving Guardian and Blocks

---
+++ Assets/Scripts/Enemies/Guardian/BlockMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BlockMove : MonoBehaviour
+{
+    private float Go, Out;
+    
+    void Update()
+    {
+        if(this.transform.position.x <= Go)
+        {
+            this.transform.position = new Vector2(Out, this.transform.position.y);
+        }
+        this.transform.Translate(Vector2.left * 2f * Time.deltaTime);
+    }
+    internal float _go
+    {
+        set {Go = value;}
+    }
+    internal float _out
+    {
+        set {Out = value;}
+    }
+}
