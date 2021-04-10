d25f2a36124f5fdf268f80313351e82d9d2b2051
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:24:46 +0700

    Created script for tracking of player and shooting

---
+++ Assets/Scripts/Enemies/Manipulator/AutoTargetLogic.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoTargetLogic : MonoBehaviour
+{   
+    [SerializeField] private GameObject Rocket = null;
+    private Transform Player = null;
+    private Animator _Animator = null;
+    private bool IsStopTarge = false;
+    private Vector2[] RocketPositions = new Vector2[6]
+    {
+        new Vector2(-7.8f, 0.38f), new Vector2(7.8f, 0.38f), new Vector2(-5.8f, 4.15f),
+        new Vector2(5.8f, 4.15f), new Vector2(-2f, 4.15f), new Vector2(2f, 4.15f),
+    };
+
+    private void Start()
+    {
+        Player = GameObject.Find("Player").GetComponent<Transform>();
+        _Animator = this.GetComponent<Animator>();
+        StartCoroutine(StopTarget(3f));
+        StartCoroutine(Fire(4.5f));
+        StartCoroutine(DeleteTarget(8f));
+    }
+    private void Update()
+    {
+        if(!IsStopTarge)
+        {
+            this.transform.position = Player.transform.position;
+        }
+    }
+
+    private IEnumerator StopTarget(float WaitingTime)
+    {
+        yield return new WaitForSeconds(WaitingTime);
+        _Animator.SetBool("IsAutoFirePhase", true);
+        IsStopTarge = true;
+    }
+    private IEnumerator Fire(float WaitingTime)
+    {
+        yield return new WaitForSeconds(WaitingTime);
+        for(int i = 0; i < 6; ++i)
+        {
+            Instantiate(Rocket, RocketPositions[i], this.transform.rotation).GetComponent<RocketMovement>()._LookAtObject = this.transform;
+        }
+    }
+    private IEnumerator DeleteTarget(float WaitingTime)
+    {
+        yield return new WaitForSeconds(WaitingTime);
+        Destroy(this.gameObject);
+    }
+}
