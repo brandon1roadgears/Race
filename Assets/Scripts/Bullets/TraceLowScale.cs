950d1ab3d83eb76c1a7cad4c03ffef1fffb0a55b
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:12:58 +0700

    Created script for changing size and deleting tracers

---
+++ Assets/Scripts/Bullets/TraceLowScale.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TraceLowScale : MonoBehaviour
+{
+    void Update()
+    {
+         this.transform.localScale = new Vector2(Mathf.Lerp(this.transform.localScale.x, 0.18f, 0.06f), Mathf.Lerp(this.transform.localScale.y, 0.18f, 0.02f));
+         if(this.transform.localScale.x < 0.2f)
+         {
+             Destroy(this.gameObject);
+         }
+    }
+}
