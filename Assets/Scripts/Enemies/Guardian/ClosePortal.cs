72850767d835f434daea8d2e48c832ecf9f7303c
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:21:45 +0700

    Created script for closing portals

---
+++ Assets/Scripts/Enemies/Guardian/ClosePortal.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ClosePortal : MonoBehaviour
+{
+    private List<Transform> BlockAndGuard;
+
+    private void Start()
+    {
+        if(BlockAndGuard == null)
+        {
+            Debug.Log("YESSSS");
+        }
+    }
+    private void Update()
+    {
+        CheckBlocksAndGuard();
+    }
+
+    private void CheckBlocksAndGuard()
+    {
+        foreach(Transform i in BlockAndGuard)
+        {
+            if(i != null)
+            {
+                return;
+            }
+        }
+        Destroy(this.gameObject);
+    }
+    internal void SetBlocksAndGuard(List<Transform> blockAndGuard, Transform Guardian)
+    {
+        BlockAndGuard = blockAndGuard;
+        BlockAndGuard.Add(Guardian);
+    }
+}
