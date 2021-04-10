b6525d0dca3c52d7da7e50702bd82a67c5d7e26c
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:16:59 +0700

    Added scriptable object field and set values from this file

--- Assets/Scripts/Enemies/Common/EnemyStats.cs
+++ Assets/Scripts/Enemies/Common/EnemyStats.cs
@@ -1,22 +1,30 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+using ScriptableStatsObject;
 
 public class EnemyStats : MonoBehaviour
 {
-    [SerializeField] private short HealthPoint = 0;
-    [SerializeField] private int PlusScore = 0;
-    [SerializeField] private GameObject DestroyAnimation = null;
-    private Text Score = null;
-    private SpawnBehaviour SpaBeh = null;
-    private ScoreManager _ScoreManager;
-    private string[] Collisions = { "PlayerBullet" };
+    [SerializeField] private EnemyStatsScOb _EnemyStats = null;
+    private SpawnBehaviour _SpawnBehaviour = null;
+    private ScoreManager _ScoreManager = null;
+    private GameObject DestroyAnimation = null;
+    private Color DestroyColor;
+    private int HealthPoint = 0;
+    private int Reward = 0;
     internal int MyPosition = 0;
+    private string[] Collisions;
     
-    private void Start()
+    private void Awake()
     {
-        Score = GameObject.Find("Text(Points)").GetComponent<Text>();
-        SpaBeh = Camera.main.GetComponent<SpawnBehaviour>();
+        _SpawnBehaviour = Camera.main.GetComponent<SpawnBehaviour>();
         _ScoreManager = Camera.main.GetComponent<ScoreManager>();
+        this.gameObject.AddComponent<SpriteRenderer>().sprite = _EnemyStats.enemySprite;
+        this.gameObject.AddComponent<Animator>().runtimeAnimatorController = _EnemyStats.controller;
+        this.gameObject.AddComponent<BoxCollider2D>().isTrigger = true;
+        DestroyAnimation = _EnemyStats.destroyAnimation;
+        HealthPoint = _EnemyStats.healthPoint;
+        Reward = _EnemyStats.reward;
+        Collisions = _EnemyStats.collisions;
+        DestroyColor = _EnemyStats.destroyColor;
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -30,10 +38,19 @@ public class EnemyStats : MonoBehaviour
         }
         if (HealthPoint == 0)
         {
-            _ScoreManager.AddMultiplier(PlusScore);
-            Instantiate(DestroyAnimation, this.transform.localPosition, this.transform.localRotation);
-            SpaBeh.SetFreePlaceInArray(MyPosition, this.gameObject.name[0]);
+            _ScoreManager.AddMultiplier(Reward);
+            Instantiate(DestroyAnimation, this.transform.position, this.transform.rotation).GetComponent<SpriteRenderer>().color = DestroyColor;
+            _SpawnBehaviour.SetFreePlaceInArray(MyPosition, this.gameObject.name[0]);
             Destroy(this.gameObject);
         }
+    }
+    internal void HealingFromDoc()
+    {
+        ++HealthPoint;
+    }
+
+    internal EnemyStatsScOb enemyStats
+    {
+        get {return _EnemyStats;}
     }
 }
