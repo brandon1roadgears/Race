b77fa6f5a4723ad91c5dee1dc5a96aea625da546
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:22:11 +0700

    Created script for moving Guardian to center

---
+++ Assets/Scripts/Enemies/Guardian/MoveToCenter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MoveToCenter : MonoBehaviour
+{
+    private SpawnBlocks _SpawnBlocks = null;
+    private float Speed = 0.05f;
+    
+    void Start()
+    {
+        _SpawnBlocks = this.GetComponent<SpawnBlocks>();
+    }
+
+    void Update()
+    {
+        if(this.transform.position.x > 0)
+        {
+            GoToCenter();
+        }
+        else
+        {
+            this.GetComponent<Animator>().SetBool("IsInGuardMod", true);
+            _SpawnBlocks.enabled = true;
+            this.transform.position = new Vector2(0.0f, this.transform.position.y);
+            this.enabled = false;
+        }
+    }
+
+    private void GoToCenter()
+    {
+        this.transform.Translate(Vector2.left * Speed);
+    }
+}
