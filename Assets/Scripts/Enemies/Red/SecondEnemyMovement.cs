5ca80714636814a405dc175d68664d992e79d467
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:38:00 +0700

    Move this script in other folder

---
+++ Assets/Scripts/Enemies/Red/SecondEnemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondEnemyMovement : MonoBehaviour
+{
+    float WaitingTime = 1.0f;
+    private bool IsMoving = true;
+    private float MoveDir = 0.0f;
+    private float [] Rnd = {-0.5f, 0.5f};
+    private Camera Cam = null;
+
+    private void Start()
+    {
+        Cam = Camera.main;
+        float LeftBorder = Cam.ScreenToWorldPoint(Cam.transform.position).x;
+        float RightBorder = -Cam.ScreenToWorldPoint(Cam.transform.position).x;
+        MoveDir = Rnd[Mathf.RoundToInt(Random.Range(0, 2))];
+
+        if (LeftBorder >= transform.position.x + MoveDir || RightBorder <= transform.position.x + MoveDir) 
+        {
+            MoveDir = MoveDir * -1;   
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (IsMoving)
+        {
+            IsMoving = false;
+            StartCoroutine(MoveEnemy2Logic());
+        }
+    }
+
+    private IEnumerator MoveEnemy2Logic()
+    {
+        yield return new WaitForSeconds(WaitingTime);
+        this.transform.position = new Vector3(this.transform.position.x + MoveDir, this.transform.position.y, this.transform.position.z);
+        MoveDir = -MoveDir;
+        IsMoving = true;
+    }
+}
