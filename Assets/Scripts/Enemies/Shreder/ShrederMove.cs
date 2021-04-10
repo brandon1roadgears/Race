3c20fb1f04175b8f3a282e2ccb7bc6378a250120
Danya <brandon1roadgears@users.noreply.github.com>
2021-04-11 00:35:06 +0700

    Created script for moving Shreder to Player

---
+++ Assets/Scripts/Enemies/Shreder/ShrederMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShrederMove : MonoBehaviour
+{
+    private Transform PlayerPosition = null;
+    private float Speed = 3f;
+    private bool StopTarget = false;
+    private void Start()
+    {
+        PlayerPosition = GameObject.Find("Player").GetComponent<Transform>();
+        StartCoroutine(StopLookOnPlayer());
+    }
+
+    private void Update()
+    {
+        this.transform.Translate(-Vector2.up * Speed * Time.deltaTime);
+        if(!StopTarget)
+        {
+            this.transform.LookAt2D(-this.transform.up, PlayerPosition);
+        }
+    }
+    private IEnumerator StopLookOnPlayer()
+    {
+        yield return new WaitForSeconds(1.6f);
+        StopTarget = true;
+        GetComponent<Animator>().SetBool("InTargetPhase", true);
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "Border")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
